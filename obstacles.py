02978de550960344ce746b97a1652e783dc94062
Kent-Diens Joseph <kentdjoseph@gmail.com>
2025-02-05 03:56:14 -0500

    changed some of the variable names to lower case.

--- obstacles.py
+++ obstacles.py
@@ -2,19 +2,24 @@ import config
 
 def is_collision(x, y):
     """ Checks if a point is in the obstacle space. Returns True if the point is in the obstacle space, and False if it is not"""
-    # First 2 rectangular obstacles
-    if ((config.LEFT_RECTANGLE['BLOATED']['X'] <= x <=(config.LEFT_RECTANGLE['BLOATED']['X']+config.LEFT_RECTANGLE['BLOATED']['WIDTH']))\
-        and y <= config.LEFT_RECTANGLE['BLOATED']['LENGTH'])\
-            or ((config.MIDDLE_RECTANGLE['BLOATED']['X'] <=x <=(config.MIDDLE_RECTANGLE['BLOATED']['X'] + config.MIDDLE_RECTANGLE['BLOATED']['WIDTH']))\
-                and y >= config.MIDDLE_RECTANGLE['BLOATED']['Y']):
+    # First rectangular obstacles
+    if ((config.LEFT_RECTANGLE['bloated']['x'] <= x <=(config.LEFT_RECTANGLE['bloated']['x']+
+                                                       config.LEFT_RECTANGLE['bloated']['width']))\
+        and y <= config.LEFT_RECTANGLE['bloated']['length']):
+        return True
+    
+    # 2nd Rectangle
+    if config.MIDDLE_RECTANGLE['bloated']['x'] <=x <=(config.MIDDLE_RECTANGLE['bloated']['x'] +
+                                                        config.MIDDLE_RECTANGLE['bloated']['width'])\
+                and (y >= config.MIDDLE_RECTANGLE['bloated']['y']):
         return True
     # Circle Obstacle
     # general equation of circle: (x-center_x)^2 + (y-center_y)^2 = radius^2
-    elif (x-config.LEFT_CIRCLE['CENTER']['X'])**2+(y-config.LEFT_CIRCLE['CENTER']['X'])**2<=\
-        config.LEFT_CIRCLE['BLOATED_RADIUS']**2:
+    if (x-config.LEFT_CIRCLE['center']['x'])**2+(y-config.LEFT_CIRCLE['center']['x'])**2<=\
+        config.LEFT_CIRCLE['bloated_radius']**2:
         return True
-    # Border region outside all obstacles 
-    elif (x <=config.BLOAT or x >= (config.WINDOW_WIDTH-config.BLOAT)) or (y <=config.BLOAT or y >= (config.WINDOW_HEIGHT-config.BLOAT)):
+    # Border region outside all obstacles
+    if (x <=config.BLOAT or x >= (config.WINDOW_WIDTH-config.BLOAT)) or (y <=config.BLOAT or y >= (config.WINDOW_HEIGHT-config.BLOAT)):
         return True
     else:               # not in obstacle space
         return False
