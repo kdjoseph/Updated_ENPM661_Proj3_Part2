6e37e624b4087ee1bba91c09ab8bffa603437e6b
Kent-Diens Joseph <kentdjoseph@gmail.com>
2025-02-07 15:51:16 -0500

    removed the wheel_rpms as an argument to be passed to the draw_action_curve function, since the actions are the rpms. Also added ValueError to the except statements in the start_goal_rpm_input() function.

--- a_star_main.py
+++ a_star_main.py
@@ -18,7 +18,7 @@ def start_goal_rpm_inputs(gazebo_coord_sw=0, strt_and_rpm_input_sw=1):
                 startb_y = float(input('Enter starting point y-cordinate in mm: '))/10
                 startb_theta = float(input('Enter starting theta in degrees: '))
                 # worst case start wrt to coord at bottom-left corner: start (6,6), goal (1194, 162) or goal (1194, 338)
-            except:
+            except ValueError:
                 print('you did not enter a number, please enter only numbers')
                 continue
             start_theta = startb_theta % 360
@@ -37,7 +37,7 @@ def start_goal_rpm_inputs(gazebo_coord_sw=0, strt_and_rpm_input_sw=1):
     while goal_pt_trigger == 1:
         try:
             goalb_x, goalb_y = float(input('Enter goal point x- cordinate in mm: '))/10, float(input('Enter goal point y-cordinate in mm: '))/10
-        except:
+        except ValueError:
             print('you did not enter a number, please enter only numbers')
             continue
         if gazebo_coord_sw == 0:
@@ -58,7 +58,7 @@ def start_goal_rpm_inputs(gazebo_coord_sw=0, strt_and_rpm_input_sw=1):
         while wheel_rpm_trigger == 1:
             try:
                 RPM_L, RPM_R = float(input('Enter an rpm less than 76 for the left-wheel: ')), float(input('Enter an rpm less than 76 for the right-wheel: '))
-            except:
+            except ValueError:
                 print('you did not enter a number, please enter only numbers')
                 continue
             if (RPM_L <0 or RPM_L >75) or (RPM_R <0 or RPM_R >75):
@@ -68,7 +68,6 @@ def start_goal_rpm_inputs(gazebo_coord_sw=0, strt_and_rpm_input_sw=1):
     return (start_x, start_y, start_theta), (goal_x, goal_y), (RPM_L, RPM_R)
 
 def main():
-# def main():
     """ Main function to start and animate the algorithm search. Then start the ROS publisher node"""
     start, goal, wheel_rpm = start_goal_rpm_inputs()
     goal_x, goal_y = goal
@@ -77,14 +76,14 @@ def main():
             (RPM_R,0), (RPM_R,RPM_R), (RPM_L,RPM_R), (RPM_R,RPM_L))
     
     # a_star_duration = A_star(1, 1, 0)
-    result = pathsearch.find_path(start, goal, wheel_rpm, actions)
+    result = pathsearch.find_path(start, goal, actions)
 
     print(f"A* Algorithm Execution Time: {result['runtime']} seconds")
 
     # if optimal path is found, create animation
     if result['message'] == "Path found!":
         animation_strt_time = time.time()
-        # animation_run_flag = True
+
         # Create Pygame window
         pygame.init()
         WINDOW = pygame.display.set_mode((config.WINDOW_WIDTH, config.WINDOW_HEIGHT))
@@ -95,28 +94,28 @@ def main():
         nodes_list = result['visited_nodes'][:-1] # slicing to avoid drawing robot motion for the last node, since after it the goal is found
         if 0<len(nodes_list)<15000:
             nodes_per_frame = int(60000/5)
-            dly = 2
+            delay = 2
         elif 15000<len(nodes_list)<30000:
             nodes_per_frame = int(60000/4)
-            dly = 1
+            delay = 1
         elif 30000< len(nodes_list) < 45000:
             nodes_per_frame  = int(60000/2)
-            dly =1
+            delay =1
         elif 45000< len(nodes_list) < 60000: 
             nodes_per_frame = 45000
-            dly = 0
+            delay = 0
         if len(nodes_list) >= 60000:
             nodes_per_frame = 60000
-            dly = 0
+            delay = 0
         # Loop to draw the action from several nodes in increments.    
         for i in range(0, len(nodes_list), nodes_per_frame):
             for node in nodes_list[i:i+nodes_per_frame]:
                 for action in actions:
                     # if draw_action_curve(curve_surface, node[0], node[1], node[2], action[0], action[1], GREEN):
                     if visualization.draw_action_curve(WINDOW, node[0], node[1], node[2], action[0], action[1], config.NODES_COLOR):
-                        # WINDOW.blit(curve_surface, (0, 0))  # Blit the curve surface onto the main window
                         pygame.display.update()
-                        pygame.time.delay(dly)  # Delay between each curve
+                        pygame.time.delay(delay)  # Delay between each curve
+                    
 
         visualization.animate_optimal_path(WINDOW, result['path'])
         animation_end_time = time.time()
