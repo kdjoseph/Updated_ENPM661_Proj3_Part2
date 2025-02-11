c3238f350795ebf201532ef3d269d6c43e96b204
Kent-Diens Joseph <kentdjoseph@gmail.com>
2025-02-11 14:48:32 -0500

    added docstring at the top to describe module. Added variable for maximum wheel rpm. Re-ordered variables, and added comments to improve clarity.

--- config.py
+++ config.py
@@ -1,14 +1,30 @@
+#!/usr/bin/env python3
+""" Module for setting key constants used by other modules """
+
+# TurtleBot3 Waffle Pi SPECS from https://emanual.robotis.com/docs/en/platform/turtlebot3/features/
 ROBOT_RADIUS = (220)/10 # 220 mm robot radius converted to cm
 WHEEL_RADIUS = (66/2)/10 # wheel diameter = 66 mm converted to cm
 WHEEL_SEPARATION = (287)/10 # 287 mm converted to cm, distance from a line through the middle of one wheel to another line through the middle of the other wheel
-
+MAX_WHEEL_RPM = 75 # rpm. Calculated based on robots max linear speed of 0.26m/s
+# DISPLAY DIMENSIONS
 WINDOW_WIDTH, WINDOW_HEIGHT = 6000/10, 2000/10 # convert to cm
-GAZEBO_START_X = 500/10 # 500 mm converted to cm
+# Suggested starting-x mentioned in the assignment pdf to be used in ros gazebo implementation.
+GAZEBO_START_X = 500/10 #cm. Does not affect user inputs, only used to capture the dimensions given in the documents.
 CLEARANCE = 29/10 # 100 mm converted to cm = 10 cm
 BLOAT = ROBOT_RADIUS + CLEARANCE
-
+# ANIMATION VARIABLES
+# Colors (RGB tuples)
+BACKGROUND_COLOR = (0, 40, 255) # blue
+OBSTACLE_COLOR = (255, 30, 70) # red
+BLOATED_OBSTACLE_COLOR = (255, 255, 0) # yellow
+NODES_COLOR = (0, 100, 0) # green
+PATH_POINTS_COLOR = (0, 0, 0) # black
+PATH_COLOR = (255, 255, 255) # white
+FPS: int = 60 # frame rate for the animation.
+# PATHSEARCH VARIABLES
 THRESHOLD_X, THRESHOLD_Y, THRESHOLD_THETA = 0.5, 0.5, 30 # cm, cm, degrees
-
+GOAL_REACHED_RADIUS = 1.5 # cm
+# OBSTACLES
 LEFT_RECTANGLE = {'height': 1000/10,
                   'width': 250/10,
                   'x': GAZEBO_START_X + 1000/10,
@@ -68,11 +84,3 @@ BLOATED_BORDERS = {
     } 
 }
 
-# Colors (RGB tuples)
-BACKGROUND_COLOR = (0, 40, 255) # blue
-OBSTACLE_COLOR = (255, 30, 70) # red
-BLOATED_OBSTACLE_COLOR = (255, 255, 0) # yellow
-NODES_COLOR = (0, 100, 0) # green
-PATH_POINTS_COLOR = (0, 0, 0) # black
-PATH_COLOR = (255, 255, 255) # white
-FPS: int = 60
